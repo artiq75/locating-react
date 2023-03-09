ab9124b2e98b0bf2999fb20a4421c5219d0aebfb
Tariq Chawki <tariq.chawki@lidem.education>
2023-03-09 16:51:47 +0100

    MigrateuseState to useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,47 @@
 import Outliner from './components/Outliner/Outliner'
 import Viewport from './components/Viewport'
 import Panel from './components/Panel'
-import { createContext, useEffect, useMemo, useRef, useState } from 'react'
-import Storage, { StorageKey } from '../core/Storage'
+import { createContext, useMemo, useReducer, useRef, useState } from 'react'
+import localEventReducer, {
+  localEventReducerAction,
+  localEventInitialState
+} from './reducers/localEventReducer'
 
 export const LocalEventContext = createContext({
-  localEvents: [],
+  localEvents: localEventInitialState,
   localEventEditableID: null,
-  onEditID: (value) => {},
-  onLocalEventDelete: (value) => {}
+  onEditID: () => {},
+  onLocalEventDelete: () => {}
 })
 
 export default function App() {
-  const [localEvents, setLocalEvents] = useState([])
   const [localEventEditableID, setLocalEventEditableID] = useState(null)
+  const [{ localEvents }, dispatch] = useReducer(
+    localEventReducer,
+    localEventInitialState
+  )
   const map = useRef(null)
 
-  useEffect(() => {
-    const storage = Storage.get(StorageKey.LOCAL_EVENT)
-    if (storage) {
-      setLocalEvents(storage)
-    }
-  }, [])
-
   const handleLocalEventAdd = function (localEvent) {
-    const newLocalEvent = {
-      id: crypto.randomUUID(),
-      ...localEvent
-    }
-    setLocalEvents((state) =>
-      Storage.set(StorageKey.LOCAL_EVENT, [newLocalEvent, ...state])
-    )
+    dispatch({ type: localEventReducerAction.ADD, payload: localEvent })
   }
 
   const handleLocalEventEdit = function (localEvent) {
-    const newLocalEvents = localEvents.map((le) => {
-      if (le.id !== localEventEditableID) return le
-      return {
+    dispatch({
+      type: localEventReducerAction.EDIT,
+      payload: {
         id: localEventEditableID,
         ...localEvent
       }
     })
-    setLocalEvents(Storage.set(StorageKey.LOCAL_EVENT, newLocalEvents))
   }
 
   const handleLocalEventDelete = function (localEvent) {
-    const newLocalEvents = localEvents.filter((le) => le.id !== localEvent.id)
-    setLocalEvents(Storage.set(StorageKey.LOCAL_EVENT, newLocalEvents))
+    dispatch({ type: localEventReducerAction.DELETE, payload: localEvent })
   }
 
   const handleLocalEventDeleteAll = function () {
-    setLocalEvents(Storage.set(StorageKey.LOCAL_EVENT, []))
+    dispatch({ type: localEventReducerAction.RESET })
   }
 
   const handleEditID = function (id) {
@@ -59,12 +50,11 @@ export default function App() {
 
   const value = useMemo(() => {
     return {
-      localEvents,
       localEventEditableID,
       onLocalEventDelete: handleLocalEventDelete,
       onEditID: handleEditID
     }
-  }, [localEvents, localEventEditableID])
+  }, [localEventEditableID])
 
   return (
     <>
---
+++ src/reducers/localEventReducer.js
@@ -0,0 +1,38 @@
+import Storage, { StorageKey } from '../../core/Storage'
+
+export const localEventReducerAction = Object.freeze({
+  ADD: 'add',
+  EDIT: 'edit',
+  DELETE: 'delete',
+  RESET: 'reset'
+})
+
+export const localEventInitialState = {
+  localEvents: Storage.get(StorageKey.LOCAL_EVENT) || []
+}
+
+export default function localEventReducer(state, action) {
+  switch (action.type) {
+    case localEventReducerAction.ADD:
+      const newLocalEvent = {
+        id: crypto.randomUUID(),
+        ...action.payload
+      }
+      return Storage.set(StorageKey.LOCAL_EVENT, [newLocalEvent, ...state.localEvents])
+    case localEventReducerAction.EDIT:
+      const newEditState = state.localEvents.map((localEvent) => {
+        if (localEvent.id !== action.payload.id) return localEvent
+        return localEvent
+      })
+      return Storage.set(StorageKey.LOCAL_EVENT, newEditState)
+    case localEventReducerAction.DELETE:
+      const newDeleteState = state.localEvents.filter(
+        (localEvent) => localEvent.id !== action.payload.id
+      )
+      return Storage.set(StorageKey.LOCAL_EVENT, newDeleteState)
+    case localEventReducerAction.RESET:
+      return Storage.set(StorageKey.LOCAL_EVENT, [])
+    default:
+      throw new Error(`L'action ${action.type} est inconnue!`)
+  }
+}
