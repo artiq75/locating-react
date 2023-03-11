b7dda1e4643a58a9a09995c0f5a68d9a8d7c7bbb
artiq75 <artiq75@icloud.com>
2023-03-11 12:08:48 +0100

    Forgot commit Removable class

---
+++ core/Removable.js
@@ -0,0 +1,21 @@
+/**
+ * Gère des elements supprimable
+ */
+export default class Removable {
+  removables = []
+
+  add(removable) {
+    this.removables.push(removable)
+  }
+
+  clear() {
+    for (const removable of this.removables) {
+      removable.remove()
+    }
+    this.removables = []
+  }
+
+  getAll() {
+    return this.removables
+  }
+}
